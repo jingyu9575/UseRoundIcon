4d5f564e45023d72188a2074e9c642aa1140991c
jingyu9575 <jingyu9575@hotmail.com>
2017-08-14 13:01:18 +0800

    v1.1

--- src/main/java/local/linux_2g64/qw/useroundicon/Main.java
+++ src/main/java/local/linux_2g64/qw/useroundicon/Main.java
@@ -2,44 +2,19 @@ package local.linux_2g64.qw.useroundicon;
 
 import android.content.res.XResources;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
-
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.util.Objects;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-
 import de.robv.android.xposed.IXposedHookZygoteInit;
 import de.robv.android.xposed.XSharedPreferences;
+import de.robv.android.xposed.XposedBridge;
 
 @SuppressWarnings("unused")
 public class Main implements IXposedHookZygoteInit {
 
     @Override
     public void initZygote(StartupParam startupParam) throws Throwable {
-        boolean useRound = true;
-
         XSharedPreferences prefs = new XSharedPreferences(Main.class.getPackage().getName());
-        try (InputStream is = new FileInputStream(prefs.getFile())) {
-            DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
-            Document document = documentBuilder.parse(is, null);
-            document.getDocumentElement().normalize();
-
-            NodeList nodes = document.getElementsByTagName("boolean");
-            for (int i = 0; i < nodes.getLength(); ++i) {
-                Element element = (Element) nodes.item(i);
-                if (Objects.equals(element.getAttribute("name"), "pref_use_round")) {
-                    useRound = !element.getAttribute("value").equals("false");
-                    break;
-                }
-            }
-        }
-
-        XResources.setSystemWideReplacement("android", "bool", "config_useRoundIcon", useRound);
+        if (prefs.getBoolean("pref_use_round", true) != prefs.getBoolean("pref_use_round", false))
+            XposedBridge.log("Warning: cannot read preference value");
+        XResources.setSystemWideReplacement("android", "bool", "config_useRoundIcon",
+                prefs.getBoolean("pref_use_round", true));
     }
 }
--- src/main/java/local/linux_2g64/qw/useroundicon/SettingsActivity.java
+++ src/main/java/local/linux_2g64/qw/useroundicon/SettingsActivity.java
@@ -1,10 +1,14 @@
 package local.linux_2g64.qw.useroundicon;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 
-public class SettingsActivity extends Activity {
+import java.io.File;
+
+public class SettingsActivity extends Activity implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,4 +21,33 @@ public class SettingsActivity extends Activity {
                 .commit();
     }
 
+    @SuppressLint("SetWorldReadable")
+    @SuppressWarnings("ResultOfMethodCallIgnored")
+    private void fixPermissions() {
+        File prefsDir = new File(getApplicationInfo().dataDir, "shared_prefs");
+        File prefsFile = new File(prefsDir, PreferenceManager.getDefaultSharedPreferencesName(this) + ".xml");
+        if (prefsFile.exists()) {
+            prefsFile.setReadable(true, false);
+            prefsFile.getParentFile().setExecutable(true, false);
+            prefsFile.getParentFile().getParentFile().setExecutable(true, false);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        fixPermissions();
+        PreferenceManager.getDefaultSharedPreferences(this).unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+        fixPermissions();
+    }
 }
